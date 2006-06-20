108201f54bf55701747786b18eb5b55f41368172
Elias Torres <eliast@apache.org>
2006-06-20 05:30:22 +0000

    - Making progress on Abdera server interfaces and scaffolding. - Changed hasEntity to hasOutput in ResponseContext - Make HandlerFactory pluggable - Added getInputStream to RequestContext - Added misc params/headers accessors to RequestContext - Added getBaseUri to RequestContext
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/branches/snell_sandbox@415507 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/abdera/server/RequestHandlerFactory.java
+++ server/src/main/java/org/apache/abdera/server/RequestHandlerFactory.java
@@ -1,8 +1,13 @@
 package org.apache.abdera.server;
 
+import org.apache.abdera.util.ServiceUtil;
+
 public interface RequestHandlerFactory {
   
-  public static final RequestHandlerFactory INSTANCE = null;
+  public static final String HANDLER_FACTORY = "org.apache.abdera.server.RequestHandlerFactory";
+
+  public static final RequestHandlerFactory INSTANCE = (RequestHandlerFactory) 
+  	ServiceUtil.newInstance(HANDLER_FACTORY, "");
   
   RequestHandler newRequestHandler(
     RequestContext requestContext) 
--- server/src/main/java/org/apache/abdera/server/servlet/ServletRequestContext.java
+++ server/src/main/java/org/apache/abdera/server/servlet/ServletRequestContext.java
@@ -1,7 +1,13 @@
 package org.apache.abdera.server.servlet;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,7 +25,7 @@ public class ServletRequestContext
   public Method getMethod() {
     return Method.valueOf(servletRequest.getMethod());
   }
-
+  
   public URI getRequestUri() {
     URI uri = null;
     try {
@@ -31,6 +37,58 @@ public class ServletRequestContext
       uri = new URI(buf.toString());
     } catch (URISyntaxException e) {}
     return uri;
+  }
+  
+  public URI getBaseUri() {
+	// TODO: this should be done from properties
+	StringBuffer buffer = new StringBuffer("http://");
+	buffer.append(servletRequest.getServerName());
+	if (servletRequest.getLocalPort() != 80) {
+		buffer.append(":");
+		buffer.append(servletRequest.getLocalPort());
+	}
+	buffer.append(servletRequest.getServletPath());
+	try {
+		return new URI(buffer.toString());
+	} catch (URISyntaxException e) {
+		throw new RuntimeException(e);
+	}
+  }
+  
+  public String getHeader(String name) {
+	  return servletRequest.getHeader(name);
+  }
+
+  public List<String> getHeaders(String name) {
+	  return Collections.list(servletRequest.getHeaders(name));
+  }
+  
+  public List<String> getHeaderNames() {
+	  return Collections.list(servletRequest.getHeaderNames());
+  }
+  
+  public URI getPathInfo() {
+	try {
+		return new URI(servletRequest.getPathInfo());
+	} catch (URISyntaxException e) {
+		throw new RuntimeException(e);
+	}
+  }
+  
+  public String getParameter(String name) {
+	  return servletRequest.getParameter(name);
+  }
+  
+  public List<String> getParameters(String name) {
+	  return Arrays.asList(servletRequest.getParameterValues(name)); 
+  }
+  
+  public List<String> getParameterNames() {
+	  return Collections.list(servletRequest.getParameterNames());
+  }
+    
+  public InputStream getInputStream() throws IOException {
+	  return servletRequest.getInputStream();
   }
 
 }

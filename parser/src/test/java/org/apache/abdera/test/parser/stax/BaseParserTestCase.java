8e5946b6f1e7f84918d872ca9a9b7464f72d8dfd
James. M. Snell <jmsnell@apache.org>
2006-06-22 23:55:21 +0000

    Minor cleanups
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@416518 13f79535-47bb-0310-9956-ffa450edef68

--- parser/src/test/java/org/apache/abdera/test/parser/stax/BaseParserTestCase.java
+++ parser/src/test/java/org/apache/abdera/test/parser/stax/BaseParserTestCase.java
@@ -19,7 +19,6 @@ package org.apache.abdera.test.parser.stax;
 
 import junit.framework.TestCase;
 
-import org.apache.abdera.model.Feed;
 import org.apache.abdera.model.Document;
 import org.apache.abdera.model.Element;
 import org.apache.abdera.parser.Parser;

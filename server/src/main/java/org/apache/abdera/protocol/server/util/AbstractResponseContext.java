6e653a8edd0454befd4d67af997acacdc4a4f1dd
James. M. Snell <jmsnell@apache.org>
2006-08-30 22:14:06 +0000

    Move the AbstractRequestHandler, AbstractResponseContext, BaseResponseContext and EmptyResponseContext classes over the the util package, which is consistent with where these types of things are placed in the other modules. Remove the impl package
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/trunk@438671 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/abdera/protocol/server/util/AbstractResponseContext.java
+++ server/src/main/java/org/apache/abdera/protocol/server/util/AbstractResponseContext.java
@@ -15,7 +15,7 @@
 * copyright in this work, please see the NOTICE file in the top level
 * directory of this distribution.
 */
-package org.apache.abdera.protocol.server.impl;
+package org.apache.abdera.protocol.server.util;
 
 import java.util.ArrayList;
 import java.util.Date;

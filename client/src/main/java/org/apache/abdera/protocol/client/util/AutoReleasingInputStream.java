b08f471f03d43d960933f7fe731835a6aba0f25f
James. M. Snell <jmsnell@apache.org>
2006-09-08 02:00:06 +0000

    Per http://jakarta.apache.org/commons/httpclient/threading.html
    Use MultiThreadedHttpConnectionManager as recommended by the Commons HTTP Client docs.
    
    Provide an implementation of an InputStream that will auto-release the HTTPMethod 
    object.  This hasn't been plugged in anywhere yet as it has the nasty side effect of
    making all of the headers in the response also disappear.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@441333 13f79535-47bb-0310-9956-ffa450edef68

---
+++ client/src/main/java/org/apache/abdera/protocol/client/util/AutoReleasingInputStream.java
@@ -0,0 +1,50 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.abdera.protocol.client.util;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.httpclient.HttpMethod;
+
+public final class AutoReleasingInputStream 
+  extends FilterInputStream {
+
+  private final HttpMethod method;
+  
+  public AutoReleasingInputStream(HttpMethod method, InputStream in) {
+    super(in);
+    this.method = method;
+  }
+
+  @Override
+  public int read() throws IOException {
+    int r = super.read();
+    if (r == -1) method.releaseConnection();
+    return r;
+  }
+
+  @Override
+  public int read(byte[] b, int off, int len) throws IOException {
+    int r= super.read(b, off, len);
+    if (r == -1) method.releaseConnection();
+    return r;
+  }
+  
+}

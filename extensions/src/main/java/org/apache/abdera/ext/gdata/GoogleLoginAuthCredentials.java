5d6fdc598cc4147858bbe555e487fef00a19e44a
James. M. Snell <jmsnell@apache.org>
2006-09-11 04:44:25 +0000

    Provide a GoogleLoginAuthCredentials implementation of the commons client Credentials interface to make it possible to reuse the gdata auth token for multiple requests.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@442096 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extensions/src/main/java/org/apache/abdera/ext/gdata/GoogleLoginAuthCredentials.java
@@ -0,0 +1,61 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.abdera.ext.gdata;
+
+import org.apache.commons.httpclient.Credentials;
+
+/**
+ * <p>When using the GoogleLoginAuthScheme with the typical Commons Client
+ * UsernamePasswordCredentials, the AuthScheme implementation will request
+ * a new auth token from the Google server for every request.  To make it 
+ * a more efficient, clients can use GoogleLoginAuthCredentials which will
+ * perform the Google Auth once to get the auth token which will be reused
+ * for every request.</p>
+ * 
+ * <pre>
+ *    GoogleLoginAuthScheme.register();
+ *   
+ *   Client client = new CommonsClient();
+ *   
+ *   GoogleLoginAuthCredentials credentials = 
+ *     new GoogleLoginAuthCredentials(
+ *       "email", "password", "blogger");
+ *   client.addCredentials(
+ *     "http://beta.blogger.com",
+ *     null, null,
+ *     credentials);
+ *  </pre>
+ */
+public final class GoogleLoginAuthCredentials 
+  implements Credentials {
+
+  private final String auth;
+  
+  public GoogleLoginAuthCredentials(String auth) {
+    this.auth = auth;
+  }
+  
+  public GoogleLoginAuthCredentials(String id, String pwd, String service) {
+    this.auth = (new GoogleLoginAuthScheme()).getAuth(id, pwd, service);
+  }
+  
+  public String getAuth() {
+    return auth;
+  }
+  
+}

70e9e28b3f9449b73e2c878dc77811ee9a2ff390
James. M. Snell <jmsnell@apache.org>
2006-11-10 20:49:48 +0000

    Specify a namespace prefix for the feed paging extension
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/trunk@473465 13f79535-47bb-0310-9956-ffa450edef68

--- extensions/src/main/java/org/apache/abdera/ext/history/FeedPagingHelper.java
+++ extensions/src/main/java/org/apache/abdera/ext/history/FeedPagingHelper.java
@@ -31,9 +31,10 @@ import org.apache.abdera.util.iri.IRISyntaxException;
  */
 public class FeedPagingHelper {
 
+  public static final String FH_PREFIX = "fh";
   public static final String FHNS = "http://purl.org/syndication/history/1.0";
-  public static final QName COMPLETE = new QName(FHNS, "complete");
-  public static final QName ARCHIVE = new QName(FHNS, "archive");
+  public static final QName COMPLETE = new QName(FHNS, "complete", "fh");
+  public static final QName ARCHIVE = new QName(FHNS, "archive", "fh");
   
   /**
    * Returns true if the feed is "complete". According to the Feed Paging

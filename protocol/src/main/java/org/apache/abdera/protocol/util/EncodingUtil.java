4ccfa5ff84a2c42c1324b003fcb0139c72f58a8a
James. M. Snell <jmsnell@apache.org>
2007-02-06 01:24:43 +0000

    Consolidating utility classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/trunk@503948 13f79535-47bb-0310-9956-ffa450edef68

--- protocol/src/main/java/org/apache/abdera/protocol/util/EncodingUtil.java
+++ protocol/src/main/java/org/apache/abdera/protocol/util/EncodingUtil.java
@@ -17,6 +17,14 @@
 */
 package org.apache.abdera.protocol.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.InflaterInputStream;
+
 import org.apache.commons.codec.net.QCodec;
 
 public class EncodingUtil {
@@ -35,5 +43,37 @@ public class EncodingUtil {
     } catch (Exception e) {
       return value;
     }
+  }
+  
+  public enum ContentEncoding { GZIP, XGZIP, DEFLATE }
+  
+  public static OutputStream getEncodedOutputStream(OutputStream out, ContentEncoding encoding) throws IOException {
+    return getEncodedOutputStream(out, new ContentEncoding[] {encoding});
+  }
+  
+  public static OutputStream getEncodedOutputStream(OutputStream out, ContentEncoding... encodings) throws IOException {
+    for (ContentEncoding encoding:encodings) {
+      switch(encoding) {
+        case GZIP:
+          out = new GZIPOutputStream(out); break;
+        case DEFLATE:
+          out = new DeflaterOutputStream(out); break;
+      }
+    }
+    return out;
+  }
+  
+  public static InputStream getDecodingInputStream(InputStream in, String ce) throws IOException {
+    String[] encodings = CacheControlParser.splitAndTrim(ce, ",", false);
+    for (int n = encodings.length -1; n >= 0; n--) {
+      switch(ContentEncoding.valueOf(encodings[n].toUpperCase().replaceAll("-", ""))) {
+        case GZIP:
+        case XGZIP:
+          in = new GZIPInputStream(in); break;
+        case DEFLATE:
+          in = new InflaterInputStream(in); break;
+      }
+    }
+    return in;
   }
 }

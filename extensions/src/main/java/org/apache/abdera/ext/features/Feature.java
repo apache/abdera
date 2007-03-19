d3eab84494c5fa665d8dfeaf4fefc709341f364c
James. M. Snell <jmsnell@apache.org>
2007-03-19 06:25:11 +0000

    Initial implementation of the in-progress http://www.ietf.org/internet-drafts/draft-snell-atompub-feature-01.txt
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@519850 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extensions/src/main/java/org/apache/abdera/ext/features/Feature.java
@@ -0,0 +1,89 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.abdera.ext.features;
+
+import javax.xml.namespace.QName;
+
+import org.apache.abdera.factory.Factory;
+import org.apache.abdera.g14n.iri.IRI;
+import org.apache.abdera.g14n.iri.IRISyntaxException;
+import org.apache.abdera.model.Element;
+import org.apache.abdera.model.ExtensibleElementWrapper;
+
+public class Feature 
+  extends ExtensibleElementWrapper {
+  
+  public Feature(Element internal) {
+    super(internal);
+  }
+  
+  public Feature(Factory factory) {
+    super(factory, FeaturesHelper.FEATURE);
+  }
+  
+  public IRI getRef() 
+    throws IRISyntaxException {
+      String ref = getAttributeValue("ref");
+      return (ref != null) ? new IRI(ref) : null;
+  }
+  
+  public boolean getRequired() {
+    String req = getAttributeValue("required");
+    if ("yes".equals(req)) return true;
+    return false;
+  }
+  
+  public IRI getHref() 
+    throws IRISyntaxException {
+      String href = getAttributeValue("href");
+      return (href != null) ? new IRI(href) : null;
+  }
+  
+  public String getLabel() {
+    return getAttributeValue("label");
+  }
+  
+  public void setRef(String ref) 
+    throws IRISyntaxException {
+      if (ref == null) throw new IllegalArgumentException();
+      setAttributeValue("ref", (new IRI(ref)).toString());
+  }
+  
+  public void setRequired(boolean required) {
+    if (required) {
+      setAttributeValue("required","yes");
+    } else {
+      removeAttribute(new QName("required"));
+    }
+  }
+  
+  public void setHref(String href) 
+    throws IRISyntaxException {
+      if (href != null)
+        setAttributeValue("href", (new IRI(href)).toString());
+      else 
+        removeAttribute(new QName("href"));
+  }
+  
+  public void setLabel(String label) {
+    if (label != null)
+      setAttributeValue("label", label);
+    else 
+      removeAttribute(new QName("label"));
+  }
+}

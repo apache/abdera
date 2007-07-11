00c586fc723b3a95016dae31c6c06baed02fa506
James. M. Snell <jmsnell@apache.org>
2007-07-11 23:48:39 +0000

    Make it easy to create a ClientAuthSSLProtocolSocketFactory instance
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@555438 13f79535-47bb-0310-9956-ffa450edef68

--- client/src/main/java/org/apache/abdera/protocol/client/util/ClientAuthSSLProtocolSocketFactory.java
+++ client/src/main/java/org/apache/abdera/protocol/client/util/ClientAuthSSLProtocolSocketFactory.java
@@ -40,26 +40,55 @@ import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
 public class ClientAuthSSLProtocolSocketFactory 
   implements SecureProtocolSocketFactory {
 
-  private String keyStore = null;
-  private String keyStoreType = null;
-  private String keyPass = null;
   private String protocol = "TLS";
   private String kmfFactory = "ibmX509";
   private TrustManager tm = null;
   
+  private final String keyStorePass;
+  private final KeyStore ks;
+  
+  public ClientAuthSSLProtocolSocketFactory(KeyStore ks, String keyStorePass) {
+    this(ks,keyStorePass,"TLS","ibmX509",null);
+  }
+  
   public ClientAuthSSLProtocolSocketFactory(
-    String keyStore, 
-    String keyStoreType,
-    String keyPass,
-    String protocol,
+    KeyStore ks, 
+    String keyStorePass,
+    String protocol, 
     String kmfFactory,
     TrustManager tm) {
-      this.keyStore = keyStore;
-      this.keyStoreType = keyStoreType;
-      this.keyPass = keyPass;
+      if (ks == null) throw new IllegalArgumentException("Invalid Keystore");
+      this.ks = ks;
+      this.keyStorePass = keyStorePass;
       if (protocol != null) this.protocol = protocol;
       if (kmfFactory != null) this.kmfFactory = kmfFactory;
       if (tm != null) this.tm = tm;
+  }
+  
+  public ClientAuthSSLProtocolSocketFactory(
+    String keyStore, 
+    String keyStoreType,
+    String keyStorePass,
+    String protocol,
+    String kmfFactory,
+    TrustManager tm) {
+      this(
+        initKeyStore(keyStore,keyStoreType,keyStorePass),
+        keyStorePass,
+        protocol,
+        kmfFactory,tm);
+  }
+  
+  private static KeyStore initKeyStore(
+    String keyStore, 
+    String keyStoreType, 
+    String keyPass) {
+      KeyStore ks = null;
+      try {
+        ks = KeyStore.getInstance(keyStoreType);
+        ks.load(new FileInputStream(keyStore), keyPass.toCharArray());
+      } catch (Exception e) {}
+      return ks;
   }
   
   public Socket createSocket(
@@ -105,13 +134,9 @@ public class ClientAuthSSLProtocolSocketFactory
     SSLSocket socket = null;
     try {
       KeyManagerFactory kmf;
-      KeyStore ks;
-      char[] passphrase = keyPass.toCharArray();
 
       context = SSLContext.getInstance(protocol);
       kmf = KeyManagerFactory.getInstance(kmfFactory);
-      ks = KeyStore.getInstance(keyStoreType);
-      ks.load(new FileInputStream(keyStore), passphrase);
 
       TrustManager tm = (this.tm != null) ? this.tm : new X509TrustManager() {
         public void checkClientTrusted(X509Certificate[] arg0, String arg1) 
@@ -123,7 +148,7 @@ public class ClientAuthSSLProtocolSocketFactory
         }
       };
       
-      kmf.init(ks, passphrase);
+      kmf.init(ks, keyStorePass.toCharArray());
       context.init(kmf.getKeyManagers(), new TrustManager[] {tm}, null);
       
       factory = context.getSocketFactory();

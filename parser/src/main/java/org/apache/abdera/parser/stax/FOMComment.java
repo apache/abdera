acd14d2a0bb87b82a2f71ed83194e1df221e31bb
James. M. Snell <jmsnell@apache.org>
2007-07-19 19:19:08 +0000

    One of the mildly annoying bits about the XPath implementation is that the selectSingleNode selectNodes methods will return Axiom objects when an xpath uses the comment(), processing-instruction(), text(), and @attr queries.  While these are not likely to be used extensively, this change hides the Axiom objects behind an Abdera facade.
    e.g. 
    
    instead of 
    
      OMText text = (OMText)xpath.selectSingleNode("//a:title/text()",entry);
    
    we can do...
    
      TextValue tv = (TextValue)xpath.selectSingleNode("//a:title/text()",entry);
      
    
    The one downside to this is that for it to work, Axiom requires us to change the value
    of the default OMFactory implementation, which could be problematic when using Abdera with
    other Axiom based software.  We want to play nice with other software packages, so we won't 
    register the default automatically.  Instead, call FOMFactory.registerAsDefault() if/when
    you're sure you want to use the TextValue class.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@557747 13f79535-47bb-0310-9956-ffa450edef68

---
+++ parser/src/main/java/org/apache/abdera/parser/stax/FOMComment.java
@@ -0,0 +1,72 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.abdera.parser.stax;
+
+import org.apache.abdera.factory.Factory;
+import org.apache.abdera.model.Base;
+import org.apache.abdera.model.Comment;
+import org.apache.abdera.model.Element;
+import org.apache.axiom.om.OMContainer;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.impl.llom.OMCommentImpl;
+
+public class FOMComment 
+  extends OMCommentImpl 
+  implements Comment {
+
+  public FOMComment(OMContainer arg0, OMFactory arg1) {
+    super(arg0, arg1);
+  }
+
+  public FOMComment(OMContainer arg0, String arg1, OMFactory arg2) {
+    super(arg0, arg1, arg2);
+  }
+  
+  public String getText() {
+    return super.getValue();
+  }
+  
+  public void setText(String text) {
+    super.setValue(text);
+  }
+  
+  @SuppressWarnings("unchecked")
+  public <T extends Base>T getParentElement() {
+    T parent = (T)super.getParent();
+    return (T) ((parent instanceof Element) ? 
+      getWrapped((Element)parent) : parent);
+  } 
+  
+  protected Element getWrapped(Element internal) {
+    if (internal == null) return null;
+    FOMFactory factory = (FOMFactory) getFactory();
+    return factory.getElementWrapper(internal);
+  }
+  
+  public Factory getFactory() {
+    return (Factory) this.factory;
+  }
+
+  public String toString() {
+    java.io.CharArrayWriter w = new java.io.CharArrayWriter();
+    try {
+      super.serialize(w);
+    } catch (Exception e) {}
+    return w.toString();
+  }
+}

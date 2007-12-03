02fbaf9d8705c883b4fb5634c35dd15cccc2e23d
James. M. Snell <jmsnell@apache.org>
2007-12-03 22:50:50 +0000

    move the various setters from AbstractResponseContext to the ResponseContext interface and refactor to use a chainable api model.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/trunk@600717 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/abdera/protocol/server/ResponseContext.java
+++ server/src/main/java/org/apache/abdera/protocol/server/ResponseContext.java
@@ -19,22 +19,78 @@ package org.apache.abdera.protocol.server;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Date;
 
+import org.apache.abdera.i18n.io.CharUtils.Profile;
 import org.apache.abdera.protocol.Response;
+import org.apache.abdera.util.EntityTag;
 import org.apache.abdera.writer.Writer;
 
 public interface ResponseContext 
   extends Response {
 
-  public boolean hasEntity();
+  boolean hasEntity();
   
-  public void writeTo(OutputStream out) throws IOException;
+  void writeTo(OutputStream out) throws IOException;
   
-  public void writeTo(java.io.Writer javaWriter) throws IOException;
+  void writeTo(java.io.Writer javaWriter) throws IOException;
   
-  public void writeTo(OutputStream out, Writer writer) throws IOException;
+  void writeTo(OutputStream out, Writer writer) throws IOException;
   
-  public void writeTo(java.io.Writer javaWriter, Writer abderaWriter) throws IOException;
+  void writeTo(java.io.Writer javaWriter, Writer abderaWriter) throws IOException;
   
-  public void setWriter(Writer writer);
+  ResponseContext setWriter(Writer writer);
+  
+  ResponseContext removeHeader(String name);
+  
+  ResponseContext setEncodedHeader(String name, String charset, String value);
+  
+  ResponseContext setEncodedHeader(String name, String charset, String... vals);
+  
+  ResponseContext setEscapedHeader(String name, Profile profile, String value);
+  
+  ResponseContext setHeader(String name, Object value);
+  
+  ResponseContext setHeader(String name, Object... vals);
+  
+  ResponseContext addEncodedHeader(String name, String charset, String value);
+
+  ResponseContext addEncodedHeaders(String name, String charset, String... vals);
+
+  ResponseContext addHeader(String name, Object value);
+
+  ResponseContext addHeaders(String name, Object... vals);
+
+  ResponseContext setAge(long age);
+
+  ResponseContext setContentLanguage(String language);
+
+  ResponseContext setContentLength(long length);
+
+  ResponseContext setContentLocation(String uri);
+
+  ResponseContext setSlug(String slug);
+
+  ResponseContext setContentType(String type);
+
+  ResponseContext setContentType(String type, String charset);
+
+  ResponseContext setEntityTag(String etag);
+
+  ResponseContext setEntityTag(EntityTag etag);
+
+  ResponseContext setExpires(Date date);
+
+  ResponseContext setLastModified(Date date);
+
+  ResponseContext setLocation(String uri);
+
+  ResponseContext setStatus(int status);
+
+  ResponseContext setStatusText(String text);
+
+  ResponseContext setAllow(String method);
+
+  ResponseContext setAllow(String... methods);
+
 }

5f866d649aa9c23b52b5dc78bcb0859d356b9242
James. M. Snell <jmsnell@apache.org>
2007-12-12 06:00:00 +0000

    These were inadvertently deleted with the last commit
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@603474 13f79535-47bb-0310-9956-ffa450edef68

---
+++ client/src/main/java/org/apache/abdera/protocol/client/cache/Cache.java
@@ -0,0 +1,63 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.abdera.protocol.client.cache;
+
+import org.apache.abdera.protocol.client.ClientResponse;
+import org.apache.abdera.protocol.client.RequestOptions;
+
+public interface Cache 
+  extends Iterable<Object> {
+
+  public static enum Disposition {
+    STALE, FRESH, TRANSPARENT 
+  }
+  
+  /**
+   * Get the disposition of a specific item
+   */
+  Disposition disposition(
+    Object key, 
+    RequestOptions options);
+    
+  /**
+   * Get a specific item from the cache
+   */
+  CachedResponse get(
+    Object key);
+  
+  /**
+   * Clear all items from the cache
+   */
+  Cache clear();
+  
+  /**
+   * Remove a specific item from the cache
+   */
+  Cache remove(
+    Object key);
+  
+  /**
+   * Update the cached item
+   */
+  ClientResponse update(
+    Object key,
+    RequestOptions options, 
+    ClientResponse response,
+    ClientResponse cached_response);
+  
+}
---
+++ client/src/main/java/org/apache/abdera/protocol/client/cache/InMemoryCache.java
@@ -0,0 +1,73 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.abdera.protocol.client.cache;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.abdera.Abdera;
+import org.apache.abdera.protocol.client.ClientResponse;
+
+public abstract class InMemoryCache 
+  extends AbstractCache {
+
+  protected transient final Map<Object,CachedResponse> cache;
+
+  protected InMemoryCache(
+    Abdera abdera, 
+    Map<Object,CachedResponse> map) {
+      super(abdera);
+      this.cache = map;
+  }
+  
+  @Override
+  protected CachedResponse createCachedResponse(
+    ClientResponse response, 
+    Object key) 
+      throws IOException {
+    return new InMemoryCachedResponse(
+      abdera, this, key, response);
+  }
+
+  public Cache clear() {
+    cache.clear();
+    return this;
+  }
+
+  public CachedResponse get(
+    Object key) {   
+      return cache.get(key);
+  }
+
+  public Cache remove(
+    Object key) {
+      cache.remove(key);
+      return this;
+  }
+
+  protected void add(
+    Object key, 
+    CachedResponse response) {
+      cache.put(key, response);
+  }
+
+  public Iterator<Object> iterator() {
+    return cache.keySet().iterator();
+  }
+}

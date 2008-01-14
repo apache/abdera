eb2252620d78fc7ad6655e0ac5cc93b089b81e43
James. M. Snell <jmsnell@apache.org>
2008-01-14 18:48:26 +0000

    No need for this to be in it's own directory
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@611888 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/abdera/examples/simple/EntityProviderExample.java
+++ examples/src/main/java/org/apache/abdera/examples/simple/EntityProviderExample.java
@@ -15,7 +15,7 @@
 * copyright in this work, please see the NOTICE file in the top level
 * directory of this distribution.
 */
-package org.apache.abdera.examples.protocol;
+package org.apache.abdera.examples.simple;
 
 import java.util.Date;
 

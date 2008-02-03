7abc810a426fb12914feb6794eeaae2546cae195
James. M. Snell <jmsnell@apache.org>
2008-02-03 00:45:39 +0000

    escaping bug
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/trunk@617935 13f79535-47bb-0310-9956-ffa450edef68

--- extensions/json/src/main/java/org/apache/abdera/ext/json/JSONUtil.java
+++ extensions/json/src/main/java/org/apache/abdera/ext/json/JSONUtil.java
@@ -29,9 +29,7 @@ import org.apache.abdera.ext.html.HtmlHelper;
 import org.apache.abdera.ext.thread.InReplyTo;
 import org.apache.abdera.ext.thread.ThreadHelper;
 import org.apache.abdera.i18n.iri.IRI;
-import org.apache.abdera.i18n.text.UrlEncoding;
 import org.apache.abdera.i18n.text.Bidi.Direction;
-import org.apache.abdera.i18n.text.CharUtils.Profile;
 import org.apache.abdera.model.Base;
 import org.apache.abdera.model.Categories;
 import org.apache.abdera.model.Category;
@@ -397,7 +395,11 @@ if (element instanceof Text) {
              "src".equalsIgnoreCase(name) || 
              "action".equalsIgnoreCase(name))) {
          IRI base = child.getResolvedBaseUri();
-         val = UrlEncoding.encode(val.trim(),Profile.IUNRESERVED.filter(),Profile.RESERVED.filter(),Profile.IPRIVATE.filter());
+//         val = UrlEncoding.encode(
+//           val.trim(),
+//           Profile.IUNRESERVED.filter(),
+//           Profile.RESERVED.filter(),
+//           Profile.IPRIVATE.filter());
          if (base != null) val = base.resolve(val).toASCIIString();
         }
         jstream.writeQuoted(val);

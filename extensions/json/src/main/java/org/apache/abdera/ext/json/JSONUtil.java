2a7cd19865e7a6d7eca7f7b8407175030946cde6
James. M. Snell <jmsnell@apache.org>
2008-03-24 19:20:37 +0000

    grrr....
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/abdera/java/trunk@640529 13f79535-47bb-0310-9956-ffa450edef68

--- extensions/json/src/main/java/org/apache/abdera/ext/json/JSONUtil.java
+++ extensions/json/src/main/java/org/apache/abdera/ext/json/JSONUtil.java
@@ -15,6 +15,8 @@
  * copyright in this work, please see the NOTICE file in the top level
  * directory of this distribution.
  */
+package org.apache.abdera.ext.json;
+
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;

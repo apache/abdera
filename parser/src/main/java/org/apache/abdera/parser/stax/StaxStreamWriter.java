f964e51e0a0369b39359b9b8838c1c7300db3f08
James. M. Snell <jmsnell@apache.org>
2008-04-11 17:30:39 +0000

    merge all of those review changes we made to 0.4.0 branch back into the trunk and bump the version number up to 0.5.0-incubating-SNAPSHOT
    git-svn-id: https://svn.apache.org/repos/asf/incubator/abdera/java/trunk@647233 13f79535-47bb-0310-9956-ffa450edef68

--- parser/src/main/java/org/apache/abdera/parser/stax/StaxStreamWriter.java
+++ parser/src/main/java/org/apache/abdera/parser/stax/StaxStreamWriter.java
@@ -265,11 +265,11 @@ public class StaxStreamWriter
       if (value == null) return this;
       try {
         if (prefix != null) {
-          if (!prefix.equals("xml") && needToWriteNamespace(prefix, namespace))
+          if (!prefix.equals("xml"))
             writeNamespace(prefix,namespace,true);
           writer.writeAttribute(prefix, namespace, name, value);
         } else if (namespace != null) {
-          if (!namespace.equals(Constants.XML_NS) && needToWriteNamespace(prefix, namespace));
+          if (!namespace.equals(Constants.XML_NS));
             writeNamespace(prefix,namespace,true);
           writer.writeAttribute(namespace, name, value);
         } else { 
